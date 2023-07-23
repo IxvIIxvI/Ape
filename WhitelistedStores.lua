edcbfe0cb32009b61e3357cfc414bb51370645d5
dsghISDgh9s7dg9sdgdsgDSEt <100976333+IxvIIxvI@users.noreply.github.com>
2023-07-22 18:13:53 -0700

    Update WhitelistedStores.lua

--- WhitelistedStores.lua
+++ WhitelistedStores.lua
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 local Whitelist = {
     ["MeatRider_Doggy"] = { 
         Tag = "Owner", 
         Color = '#FF0202' 
-=======
-local Whitelist = {    ["NewUser3"] = {
-        Tag = "New Tag 3",
-        Color = "#AA00FF"
     },
-    ["NewUser3"] = {
-        Tag = "New Tag 3",
-        Color = "#AA00FF"
->>>>>>> 69b74b6bc0d7c912b95966fb3adb04d4da75cc4d
-    },
+    ["IIIIIlIIlIIllIlII"] = {
+        Tag = "is he blakc ???",
+        Color = "#12FF00"
+    }
 }
-
-
 
 return Whitelist
