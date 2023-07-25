6d1b45d336de3d6a32bf2dcf8b09d10dcba97565
IxvIIxvI <100976333+IxvIIxvI@users.noreply.github.com>
2023-07-25 13:47:06 -0700

    Added new variable 'New_Store' to the Lua table

--- WhitelistedStores.lua
+++ WhitelistedStores.lua
@@ -2,9 +2,13 @@ return  {
     ["MeatRider_Doggy"] = { 
         Tag = "Owner", 
         Color = '#FF0202' 
-    };
+    },
     ["IIIIIlIIlIIllIlII"] = {
         Tag = "is he blakc ???",
         Color = "#12FF00"
-    };
+    },
+}["New_Store"] = {
+    Tag = "New Tag",
+    Color = "#FF0000"
+},
 }
