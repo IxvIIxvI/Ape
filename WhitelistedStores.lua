e3df239596dc91139828424c6f50345d159623cc
IxvIIxvI <100976333+IxvIIxvI@users.noreply.github.com>
2023-07-25 13:47:55 -0700

    Update WhitelistedStores.lua

--- WhitelistedStores.lua
+++ WhitelistedStores.lua
@@ -2,13 +2,9 @@ return  {
     ["MeatRider_Doggy"] = { 
         Tag = "Owner", 
         Color = '#FF0202' 
-    },
+    };
     ["IIIIIlIIlIIllIlII"] = {
         Tag = "is he blakc ???",
         Color = "#12FF00"
-    },
-}["New_Store"] = {
-    Tag = "New Tag",
-    Color = "#FF0000"
-},
+    };
 }
