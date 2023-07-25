3a7a18a53b67062e6f76d59a4d3d5d0606450d84
dsghISDgh9s7dg9sdgdsgDSEt <100976333+IxvIIxvI@users.noreply.github.com>
2023-07-25 13:17:25 -0700

    Update WhitelistedStores.lua

--- WhitelistedStores.lua
+++ WhitelistedStores.lua
@@ -2,11 +2,11 @@ local Whitelist = {
     ["MeatRider_Doggy"] = { 
         Tag = "Owner", 
         Color = '#FF0202' 
-    },
+    };
     ["IIIIIlIIlIIllIlII"] = {
         Tag = "is he blakc ???",
         Color = "#12FF00"
-    }
+    };
 }
 
 return Whitelist
